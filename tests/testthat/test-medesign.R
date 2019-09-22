262dd24603de5035b5284a3b7a5c53c00b3e372e
Jorge Cimentada <cimentadaj@gmail.com>
2019-09-22 17:07:51 +0200

    Checks sscore output of medesign

--- tests/testthat/test-medesign.R
+++ tests/testthat/test-medesign.R
@@ -10,6 +10,7 @@ validate_medesign <- function(x) {
 
   expect_true(all(vapply(x, nrow, FUN.VALUE = numeric(1)) > 0))
   expect_true(all(vapply(x, is.data.frame, FUN.VALUE = logical(1))))
+  
   expect_s3_class(x$me_data, "me")
 }
 
@@ -95,6 +96,7 @@ test_that("medesign raises error when bad model_syntax", {
   )
 })
 
+
 test_that("medesign checks format of .data", {
   # These three variables share a common method
   me_syntax <- "~ mpg + cyl + drat"
@@ -135,6 +137,137 @@ test_that("medesign returns expected format", {
                         )
   res <- medesign(me_syntax, mtcars, me_data)
   validate_medesign(res)
+  expect_equivalent(nrow(res$parsed_model), 3)
+  expect_equivalent(unique(res$parsed_model$type), "observed")
+
+  # Make sure that the diag is replaced for the three observed variables
+  replaced_diag <- diag(as.matrix(res$corr[-1]))
+  expect_true(
+    all(names(mtcars)[which(replaced_diag != 1)] == c("mpg", "cyl", "drat"))
+  )
+
+  # Compare that the replaced diagonals are different from the original
+  # ones for the variables in me_data
+  original_diag <- diag(as.matrix(me_covariance(mtcars)[-1]))
+  replaced_diag <- diag(as.matrix(res$covv[-1]))
+  are_different <- original_diag != replaced_diag
+
+  expect_true(
+    all(names(mtcars)[which(are_different)] == c("mpg", "cyl", "drat"))
+  )
+
+})
+
+test_that("medesign replaces diag for vars in me_data despite model_syntax", {
+  # These three variables share a common method
+  me_syntax <- "~ mpg + cyl"
+  # Fake data for the example
+  me_data <- data.frame(stringsAsFactors = FALSE,
+                        question = c("mpg", "cyl", "drat"),
+                        reliability = c(0.729, 0.815, 0.68),
+                        validity = c(0.951, 0.944, 0.79),
+                        quality = c(0.693, 0.77, 0.89)
+                        )
+  res <- medesign(me_syntax, mtcars, me_data)
+
+  # Make sure that the diag is replaced for the three observed variables
+  # in me_data even if the model_syntax only declares two
+  replaced_diag <- diag(as.matrix(res$corr[-1]))
+  expect_true(
+    all(names(mtcars)[which(replaced_diag != 1)] == c("mpg", "cyl", "drat"))
+  )
+
+  # Compare that the replaced diagonals are different from the original
+  # ones for the variables in me_data
+  original_diag <- diag(as.matrix(me_covariance(mtcars)[-1]))
+  replaced_diag <- diag(as.matrix(res$covv[-1]))
+  are_different <- original_diag != replaced_diag
+
+  expect_true(
+    all(names(mtcars)[which(are_different)] == c("mpg", "cyl", "drat"))
+  )
+})
+
+test_that("medesign creates sscore and removes vars from me_data", {
+  # These three variables share a common method
+  me_syntax <- "sscore_one = drat + disp; ~ mpg + cyl"
+  # Fake data for the example
+  me_data <- data.frame(stringsAsFactors = FALSE,
+                        question = c("mpg", "cyl", "drat", "disp"),
+                        reliability = c(0.729, 0.815, 0.68, 0.8),
+                        validity = c(0.951, 0.944, 0.79, 0.92),
+                        quality = c(0.693, 0.77, 0.89, 0.97)
+                        )
+  res <- medesign(me_syntax, mtcars, me_data)
+  mod <- res$parsed_model
+  sscore_names <- mod[mod$op == "=", 1]
+
+  # sscore columns is created
+  expect_true(
+    sscore_names %in% names(res$.data)
+  )
+
+  # sscore columns is not empty
+  expect_true(
+    all(!is.na(res$.data[sscore_names]))
+  )
+
+  # sscore column is in corr/cov
+  expect_true(all(sscore_names %in% names(res$corr)))
+  expect_true(all(sscore_names %in% res$corr[["rowname"]]))
+  expect_true(all(sscore_names %in% names(res$covv)))
+  expect_true(all(sscore_names %in% res$covv[["rowname"]]))
+
+  # create sscore exclude variables from me_data
+  expect_true(
+    all(!(c("drat", "disp") %in% res$me_data$question))
+  )
+
+})
+
+test_that("medesign creates two sscore and removes vars from me_data", {
+  # These three variables share a common method
+  me_syntax <- "sscore_one = drat + disp; sscore_two = am + gear; ~ mpg + cyl"
+  # Fake data for the example
+  me_data <- data.frame(stringsAsFactors = FALSE,
+                        question = c("mpg", "cyl", "drat", "disp", "am", "gear"), #nolintr
+                        reliability = c(0.729, 0.815, 0.68, 0.8, 0.79, 0.98),
+                        validity = c(0.951, 0.944, 0.79, 0.92, 0.45, 0.78),
+                        quality = c(0.693, 0.77, 0.89, 0.97, 0.70, 0.49)
+                        )
+
+  res <- medesign(me_syntax, mtcars, me_data)
+  mod <- res$parsed_model
+  sscore_names <- mod[mod$op == "=", 1]
+
+  # sscore columns is created
+  expect_true(
+    all(sscore_names %in% names(res$.data))
+  )
+
+  # sscore columns is not empty
+  expect_true(
+    all(!is.na(res$.data[sscore_names]))
+  )
+
+  # sscore column is in corr/cov
+  expect_true(all(sscore_names %in% names(res$corr)))
+  expect_true(all(sscore_names %in% res$corr[["rowname"]]))
+  expect_true(all(sscore_names %in% names(res$covv)))
+  expect_true(all(sscore_names %in% res$covv[["rowname"]]))
+
+  # create sscore exclude variables from me_data
+  expect_true(
+    all(!(c("drat", "disp") %in% res$me_data$question))
+  )
+
+  # create sscore exclude variables from me_data
+  expect_true(
+    all(!(c("drat", "disp", "am", "gear") %in% res$me_data$question))
+  )
+
+})
+
 test_that("mdesign only checks for NA in me_data for used variables", {
   # These three variables share a common method
   me_syntax <- "~ mpg + cyl"
