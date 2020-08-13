6e8acfa4d04900e170d6a41d375857d746f2793d
Jorge Cimentada <cimentadaj@gmail.com>
2020-08-13 17:14:07 +0200

    Adds ess7es as data to the package

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Data from the European Social Survey, Round 7 for Spain
+#'
+#' A dataset containing some variables from the European Social Survey,
+#' Round 7 for Spain.
+#'
+#' @format A data frame with 1925 rows and 11 variables:
+#' \describe{
+#' \item{trstprl}{How much political trust do you have in institutions/parliament}
+#' \item{trstplt}{How much political trust do you have in authorities/politicians}
+#' \item{trstprt}{How much political trust do you have in institutions/political parties}
+#' \item{stfedu}{How much political satisfaction do you have in education}
+#' \item{stfhlth}{How much political satisfaction do you have in health services}
+#' \item{psppsgv}{Political system allows people to have a say in what government does}
+#' \item{psppipl}{Political system allows people to have influence on politics}
+#' \item{ptcpplt}{Politicians care what people think}
+#' \item{ppltrst}{Social trust}
+#' \item{polintr}{Political interest}
+#' \item{stflife}{Subjective wellbeing , life satisfaction}
+#' \item{stfeco}{Political satisfaction , country's economy}
+#' \item{agea}{Age}
+#' \item{edulvlb}{Education level}
+#' }
+#' @source Downloaded from the ESS website at https://www.europeansocialsurvey.org/download.html?file=ESS7ES&c=ES&y=2014
+"ess7es"
