9ab5eb3b1a3c8fcc0b9bdc41c7b65dda6a366dd8
Jorge Cimentada <cimentadaj@gmail.com>
2020-12-15 13:28:18 +0100

    Adds complex weight related variables to ESS data

--- R/data.R
+++ R/data.R
@@ -3,8 +3,9 @@
 #' A dataset containing selected variables from the European Social Survey,
 #' Round 7 for Spain.
 #'
-#' @format A data frame with 1925 rows and 11 variables:
+#' @format A data frame with 1925 rows and 19 variables:
 #' \describe{
+#' \item{idno}{The unique ID of each respondent}
 #' \item{trstprl}{How much political trust do you have in institutions/parliament}
 #' \item{trstplt}{How much political trust do you have in authorities/politicians}
 #' \item{trstprt}{How much political trust do you have in institutions/political parties}
@@ -20,7 +21,9 @@
 #' \item{agea}{Age}
 #' \item{edulvlb}{Education level}
 #' \item{eisced}{ISCED education classification}
-#' \item{pspwght}{Weights}
+#' \item{psu}{Primary sampling unit. This is useful for doing complex weighted analysis.}
+#' \item{stratum}{The stratum of each respondent. This is useful for doing complex weighted analysis}
+#' \item{prob}{Probability weights. This is useful for doing complex weighted analysis}
 #' }
-#' @source Downloaded from the ESS website at https://www.europeansocialsurvey.org/download.html?file=ESS7ES&c=ES&y=2014
+#' @source Downloaded from the ESS website at https://www.europeansocialsurvey.org/download.html?file=ESS7ES&c=ES&y=2014 and merged with SDDF data for the weight variables.
 "ess7es"
